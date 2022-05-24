25e9a03913cdf77df74cd34bf4179c83b87ac12e
aeseb <sebastoledano@outlook.com>
2022-05-24 16:18:50 -0300

    script

---
+++ algoritmo.js
@@ -0,0 +1,30 @@
+document.write(`<h2><details>
+<summary><b>Poema.</b></summary>
+<p>
+<b>
+    Los Justos.
+    <br>
+    <br>
+    Un hombre que cultiva su jardín, como quería Voltaire.<br>
+    El que agradece que en la tierra haya música.<br>
+    El que descubre con placer una etimología.<br>
+    Dos empleados que en un café del Sur juegan un silencioso ajedrez.<br>
+    El ceramista que premedita un color y una forma.<br>
+    El tipógrafo que compone bien esta página, que tal vez no le agrada.<br>
+    Una mujer y un hombre que leen los tercetos finales de cierto canto.<br>
+    El que acaricia a un animal dormido.<br>
+    El que justifica o quiere justificar un mal que le han hecho.<br>
+    El que agradece que en la tierra haya Stevenson.<br>
+    El que prefiere que los otros tengan razón.<br>
+    Esas personas, que se ignoran, están salvando el mundo.<br>
+</b>   
+</p>
+
+</details></h2>
+<h2><details>
+<summary><b>Autor.</b></summary>
+<p>
+    <img title="Jorge Luis Borges" width="700" height="500" src="https://s1.eestatic.com/2018/01/10/cultura/libros/libros_276233528_59961615_1706x1280.jpg" alt="">
+    <a href="https://es.wikipedia.org/wiki/Jorge_Luis_Borges">Wikipedia</a>
+</p>
+</details></h2>`);
